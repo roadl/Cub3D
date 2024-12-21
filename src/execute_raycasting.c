6eaf15cb2d279bbef28c9ca0c823d1dd9bef6aa2
yuyu <yuyunjae03@gmail.com>
2024-12-21 20:15:11 +0900

    fix hit cal

--- src/execute_raycasting.c
+++ src/execute_raycasting.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execute.raycasting.c                               :+:      :+:    :+:   */
+/*   execute_raycasting.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yuyu <yuyu@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 16:21:59 by yuyu              #+#    #+#             */
-/*   Updated: 2024/12/21 19:51:00 by yuyu             ###   ########.fr       */
+/*   Updated: 2024/12/21 20:14:51 by yuyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,43 +87,6 @@ void	dda_algorithm(t_mlx *mlx)
 	}
 }
 
-void	cal_hit_pos(t_mlx *mlx)
-{
-	if (mlx->ray.dy > 0 && mlx->hit.hit_dir == -3)
-		mlx->hit.x_pos = 1 - mlx->hit.x_pos;
-	if (mlx->ray.dx > 0 && mlx->hit.hit_dir == -2)
-		mlx->hit.x_pos = 1 - mlx->hit.x_pos;
-
-	printf("texture coordinate: %lf", mlx->hit.x_pos);
-	if (mlx->hit.x_pos < 0)
-		mlx->hit.x_pos = 0;
-	else if (mlx->hit.x_pos > 1)
-		mlx->hit.x_pos = 1;
-}
-
-void	cal_hit_dir(t_mlx *mlx)
-{
-
-}
-
-void	cal_hit_parameter(t_mlx *mlx)
-{
-	if (mlx->hit.hit_dir == -2)
-	{
-		mlx->hit.distance = mlx->ray.x_dist - mlx->ray.delta_x;
-		mlx->hit.x_pos = mlx->info.person.y_pos + mlx->ray.dy * mlx->hit.distance - mlx->ray.y_pos;
-	}
-	if (mlx->hit.hit_dir == -3)
-	{
-		mlx->hit.distance = mlx->ray.y_dist - mlx->ray.delta_y;
-		mlx->hit.x_pos = mlx->info.person.x_pos + mlx->ray.dx * mlx->hit.distance - mlx->ray.x_pos;
-	}
-	else
-		error_occur(mlx, "hit error", 1);
-	cal_hit_pos(mlx);
-	cal_hit_dir(mlx);
-}
-
 void	ray_casting(t_mlx *mlx)
 {
 	int x;
@@ -138,8 +101,8 @@ void	ray_casting(t_mlx *mlx)
 	}
 }
 
-void	render_sceen(t_mlx *mlx) // 미완
+void	render_sceen(t_mlx *mlx)
 {
 	ray_casting(mlx);
-	draw_sceen(mlx);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img.img_ptr, 0, 0);
 }
